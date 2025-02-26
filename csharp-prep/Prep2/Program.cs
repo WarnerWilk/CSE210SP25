e9acef522583266a5293a118a8070610621e3ca1
WarnerWilk <wil20093@byui.edu>
2025-02-26 10:00:19 -0700

    Began work on assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your percent grade to the nearest integer?");
+        string userInput = Console.ReadLine();
+        int persentGrade = int.Parse(userInput);
     }
 }
