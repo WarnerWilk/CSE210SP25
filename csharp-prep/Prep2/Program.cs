acab0b839ad09afa53a5dd924ce3aed891c5ce69
WarnerWilk <wil20093@byui.edu>
2025-02-27 16:32:03 -0700

    I finished the program. It accepts a number grade and then converts it to a letter grade, printing the letter by use of a variable.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,9 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
-        Console.WriteLine("What is your percent grade to the nearest integer?");
+        Console.WriteLine("What is your percent grade?");
         string userInput = Console.ReadLine();
-        int persentGrade = int.Parse(userInput);
-        
+        float percentGrade = float.Parse(userInput);
+        string letter = "A";
+        if(percentGrade >=80 && percentGrade < 90)
+        {
+            letter = "B";
+        }
+        else if(percentGrade >=70 && percentGrade < 80)
+        {
+            letter = "C";
+        }
+        else if(percentGrade < 70)
+        {
+            letter = "D";
+            Console.WriteLine($"Hmm. You didn't pass. You'll do better next time!");
+        }
+        Console.WriteLine($"You got an {letter}!");
     }
 }
