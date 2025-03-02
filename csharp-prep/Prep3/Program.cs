10b38506f5f1f0034ff8c0a7c09d8ed183673695
WarnerWilk <wil20093@byui.edu>
2025-03-01 22:14:29 -0700

    Wrote the working program. Heavily consulted previous Python code from previous class.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,51 @@
 using System;
 
+using System;
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        string again;
+
+        do
+        {
+            int no = random.Next(1, 101);
+            int guess = 0;
+            int number_of_guesses = 1;
+
+            Console.WriteLine("Welcome to the guessing game!");
+            
+            do
+            {
+                Console.Write("What is your guess?\n");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > no)
+                {
+                    Console.WriteLine("It's lower");
+                }
+                else if (guess < no)
+                {
+                    Console.WriteLine("It's higher");
+                }
+                
+                if (guess != no)
+                {
+                    number_of_guesses++;
+                }
+            }
+            while (guess != no);
+
+            Console.WriteLine("That's right!");
+            Console.WriteLine($"It took you {number_of_guesses} tries to guess the right number!");
+
+            Console.Write("Would you like to try again? (yes/no)\n");
+            again = Console.ReadLine().ToLower();
+        }
+        while (again != "no");
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
