bea6e0006ecdd5a1219039bf8ee66275e2ae7d40
WarnerWilk <wil20093@byui.edu>
2025-03-01 22:56:06 -0700

    Completed the assignment. No problems again. Wrote new functions that prompt the user for information and then manipulates the input and displays it again.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
+    static void personalMessage(string userName, int userNumber)
+    {
+        Console.WriteLine($"Hello there {userName}!");
+        Console.WriteLine($"The square of your number is {userNumber * userNumber}.");
+    }
+    static int promptUserNumber()
+    {
+        int userNumber;
+        Console.WriteLine($"What is your favorite integer?");
+        userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+    static string promptUserName()
+    {
+        string userName;
+        Console.WriteLine($"Hello! What is your name?");
+        userName = Console.ReadLine();
+        return userName;
+    }
     static void Main(string[] args)
     {
+        string userName;
+        int userNumber;
         Console.WriteLine("Hello Prep5 World!");
+        userName = promptUserName();
+        userNumber = promptUserNumber();
+        personalMessage(userName,userNumber);
     }
 }
