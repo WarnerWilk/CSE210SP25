ec61244596f99591729920a00d62ef304525ed8e
WarnerWilk <wil20093@byui.edu>
2025-04-24 09:20:50 -0600

    Commented out the circle code.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,6 +8,12 @@ class Program
         Console.WriteLine("Hello Warner's World!");
         Console.WriteLine("Hello. Shall we play a game?");
 
+        // // double x = 10;
+        // Circle myCircle = new Circle(10);
+        // // Circle myCircle = new Circle(x);
+        // Console.WriteLine($"The area of the circle is {myCircle.GetArea()}");
+        // Circle myCircle2 = new Circle(20);
+        // Console.WriteLine($"The are of the new circle is {myCircle2.GetArea()}");
 
 
     }
