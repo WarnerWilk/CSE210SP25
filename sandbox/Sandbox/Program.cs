b3a654d2701a5216594dbfa90db0bf0f6eb31322
WarnerWilk <wil20093@byui.edu>
2025-04-29 09:34:30 -0600

    Practice with "FOR" loops.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -13,8 +13,16 @@ class Program
         // Circle myCircle = new Circle(x);
         Console.WriteLine($"The area of the circle is {myCircle.GetArea()}");
         Circle myCircle2 = new Circle(20);
-        Console.WriteLine($"The are of the new circle is {myCircle2.GetArea()}");
+        Console.WriteLine($"The area of the new circle is {myCircle2.GetArea()}");
 
+        int x = 16;
+        if(x==16)
+        {
+            Console.WriteLine("X is 16");
+        }
+        for(int i= 0; i < x; i++) {
+            Console.WriteLine("Here come the drums");
+            }
 
     }
 }
