c5271409a421d56d3dbf2a09b59deba9d2ed7dfa
WarnerWilk <wil20093@byui.edu>
2025-05-15 10:01:25 -0600

    Started project over from last semester. Created menu class, and it is fully functioning.

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,28 @@
+
+// public void WriteToFile(string filename)
+// {
+//     using (StreamWriter outputFile = new StreamWriter(filename))
+//     {
+//         foreach(JournalEntry entry in entries)
+//         {
+//             outputFile filename
+//         }
+//     }
+// }
+
+// public void ReadFromFile(string filename)
+// {
+//     string[] lines = System.IO.File.ReadAllLines(filename);
+
+//     foreach (string line in lines)
+//     {
+//         string[] parts = line.Split("#");
+
+//         string date = parts[0];
+//         string questions = parts[1];
+//         string entryText = parts[2];
+
+//         JournalEntry entry = new JournalEntry();
+//         entry.Create
+//     }
+// }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,11 +1,81 @@
 using System;
+using System.ComponentModel.Design;
+using System.Xml.Serialization;
 
-class Program
+// class Program
+// {
+
+//     private static string ObtainFileName(string prompt)
+//     {
+//         Console.WriteLine(prompt);
+//         string fileName = Console.ReadLine();
+//         return fileName;
+//     }
+class Menu
 {
-    static void Main(string[] args)
+    public void DisplayMenu()
     {
-        Console.WriteLine("Hello Develop02 World!");
-        Menu menu = new Menu();
-        menu.DisplayMenu();
+        Console.WriteLine("Please select on of the following options:");
+        Console.WriteLine("1. Write to journal");
+        Console.WriteLine("2. Display loaded journal");
+        Console.WriteLine("3. Save journal to file");
+        Console.WriteLine("4. Load journal");
+        Console.WriteLine("5. Exit program");
+
+    }
+    public void MenuSelect()
+    {
+            string input;
+            int choice = 0;
+        while (choice != 5)
+        {
+            input = Console.ReadLine();
+            choice = Int32.Parse(input);
+            switch (choice)
+            {
+                case 1:
+                    // Journal.WriteToFile();
+                    Console.WriteLine("Choice 1 detected.");
+                    break;
+                case 2:
+                    // Journal.Display();
+                    Console.WriteLine("Choice 2 detected.");
+                    break;
+                case 3:
+                    // Journal.SaveToFile();
+                    Console.WriteLine("Choice 3 detected.");
+                    break;
+                case 4:
+                    // Journal.LoadFromFile();
+                    Console.WriteLine("Choice 4 detected.");
+                    break;
+                case 5:
+                    Console.WriteLine("Exiting program.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input, please try again.");
+                    break;
+            }
+        }
     }
 }
+
+class Program()
+{
+    static void Main()
+    {
+        Menu menu = new Menu();
+        menu.DisplayMenu();
+        menu.MenuSelect();
+
+    }
+}
+
+//     static void Main(string[] args)
+//     {
+//         Journal newJournal = new Journal();
+//         JournalQuestions journalQuestions = new JournalQuestions();
+
+//         MenuCommand menu = new Menu()
+//     }
+// }
