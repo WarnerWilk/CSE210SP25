fa19aea40c92d51a0a1d79e450edd050b64fce5e
WarnerWilk <wil20093@byui.edu>
2025-05-22 12:43:30 -0600

    Created Fraction class. All constructors, getters, and setters working properly.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction testFraction1 = new Fraction();
+        Console.WriteLine(testFraction1.GetFractionString());
+        Fraction testFraction2 = new Fraction(5);
+        Console.WriteLine(testFraction2.GetFractionString());
+        Fraction testFraction3 = new Fraction(6, 8);
+        Console.WriteLine(testFraction3.GetFractionString());
+        testFraction1.SetNumerator(5);
+        testFraction1.SetDenominator(3);
+        Console.WriteLine(testFraction1.GetFractionString());
     }
 }
---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+        return;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _numerator = wholeNumber;
+        _denominator = 1;
+    }
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+    public void SetNumerator(int numerator)
+    {
+        _numerator = numerator;
+    }
+    public int GetDenominator()
+    {
+        return _denominator;
+    }
+    public void SetDenominator(int denominator)
+    {
+        _denominator = denominator;
+    }
+
+    public string GetFractionString()
+    {
+        return ($"{_numerator}/{_denominator}");
+    }
+    public double GetDecimalValue()
+    {
+        return (_numerator / _denominator);
+    }
+
+}
