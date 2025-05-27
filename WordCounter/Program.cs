198de5c0064b490b252464d7c06b6b7ac55d4004
WarnerWilk <wil20093@byui.edu>
2025-05-27 09:55:55 -0600

    Initial running wordcounter

---
+++ WordCounter/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        WordCounter wordCounter = new WordCounter("The quick brown fox jumped over the lazy dog.");
+        wordCounter.DisplayWords();
+    }
+}
