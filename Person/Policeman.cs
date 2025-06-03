7d898866c713274e5812b02482b3663b3cb2106b
WarnerWilk <wil20093@byui.edu>
2025-06-03 09:58:41 -0600

    Policeman class getters and setters working. GetPersonInformation working on the Policeman class as well.

---
+++ Person/Policeman.cs
@@ -0,0 +1,8 @@
+class Policeman : Person
+{
+    public Policeman(string firstName, string lastName, int age) 
+    : base(firstName, lastName, age)
+    {
+        
+    }
+}
--- Person/Program.cs
+++ Person/Program.cs
@@ -3,6 +3,10 @@
     public static void Main(string[] args)
     {
         Person myPerson = new Person("Bubba", "Bob", 53);
+
         Console.WriteLine(myPerson.GetPersonInformation());
+
+        Policeman myPoliceman = new Policeman("Cooper", "Steves", 30);
+        Console.WriteLine(myPoliceman.GetPersonInformation());
     }
 }
